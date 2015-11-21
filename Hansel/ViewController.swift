e17dd5854aeb4a048208a415c7713f4774304186
Marc Pechaitis <marcpechaitis@gmail.com>
2015-11-20 17:54:19 -0800

    Initial Commit

---
+++ Hansel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hansel
+//
+//  Created by Marc Pechaitis on 11/20/15.
+//  Copyright © 2015 Marc Pechaitis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
