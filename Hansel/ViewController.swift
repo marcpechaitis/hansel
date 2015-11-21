4223d0f63726c4b88aa1dccb113d131e7cb94a5f
Marc Pechaitis <marcpechaitis@gmail.com>
2015-11-20 19:23:48 -0800

    created branch Sandbox for playing around

--- Hansel/ViewController.swift
+++ Hansel/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var viewForLayer: UIView!
+  
+  var l: CALayer {
+    return viewForLayer.layer
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    setUpLayer()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func setUpLayer() {
+    l.backgroundColor = UIColor.blueColor().CGColor
+    l.borderWidth = 100.0
+    l.borderColor = UIColor.redColor().CGColor
+    l.shadowOpacity = 0.7
+    l.shadowRadius = 10.0
   }
-
-
+  
 }
-
